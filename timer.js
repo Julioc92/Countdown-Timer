487c76a8359546f6b22b6634c9bd93394beb1a53
Julio Perez <57919712+Julioc92@users.noreply.github.com>
2021-02-21 20:00:35 -0500

    timer

--- timer.js
+++ timer.js
@@ -1,17 +1,19 @@
 
     const timeLeftDisplay = document.querySelector('#time-left')
     const startBtn = document.querySelector('#start-button')
+   
     let timeLeft = 10
 
     function countDown(){
-        setInterval(function(){
-            if(timeLeft <= 0 ){
+        setInterval(function(){  // function loop 
+            if(timeLeft <= 0 ){  // this will stop the count at zero
                clearInterval(timeLeft = 0)
             }
-
+            // it will loop minus one every 1000 milliseconds
             timeLeftDisplay.innerHTML = timeLeft
             timeLeft -=1
-        }, 1000)
+        }, 1000) 
     }
 
+    // start the count down on a click
     startBtn.addEventListener('click', countDown)
